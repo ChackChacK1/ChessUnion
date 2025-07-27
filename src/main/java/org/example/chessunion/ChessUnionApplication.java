6ff8fa7845bf3cbd4e5893278c6c5cb0c3af0acd
ChackChacK1 <fworld183@gmail.com>
2025-07-28 00:00:27 +0300

    init commit

---
+++ src/main/java/org/example/chessunion/ChessUnionApplication.java
@@ -0,0 +1,13 @@
+package org.example.chessunion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessUnionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessUnionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chessunion/ChessUnionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chessunion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessUnionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
